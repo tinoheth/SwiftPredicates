fccb430a2cff45c0ec710ac524cac643df1d0d29
Tino Heth <th@t-no.de>
2015-01-10 19:11:30 +0100

    Initial Commit

---
+++ NSPredicateSwifted/NSPredicateSwifted.h
@@ -0,0 +1,19 @@
+//
+//  NSPredicateSwifted.h
+//  NSPredicateSwifted
+//
+//  Created by Tino Heth on 10.01.15.
+//  Copyright (c) 2015 Tino Heth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NSPredicateSwifted.
+FOUNDATION_EXPORT double NSPredicateSwiftedVersionNumber;
+
+//! Project version string for NSPredicateSwifted.
+FOUNDATION_EXPORT const unsigned char NSPredicateSwiftedVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NSPredicateSwifted/PublicHeader.h>
+
+
